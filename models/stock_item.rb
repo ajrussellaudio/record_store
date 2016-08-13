a2da206f72f6ef6d3ae3c9a52880faf03a3b250c
Alan Russell <ajrussellaudio@gmail.com>
2016-08-13 19:31:19 +0100

    initializes stock model

---
+++ models/stock_item.rb
@@ -0,0 +1,15 @@
+require_relative "../db/sql_runner"
+
+class StockItem
+
+  def initialize( options )
+    @id         = options['id'].to_i
+    @album_id   = options['id'].to_i
+    @format     = options['format']
+    @stock      = options['stock'].to_i
+    @threshold  = options['threshold'].to_i
+    @buy_price  = options['buy_price'].to_i
+    @sell_price = options['sell_price'].to_i
+  end
+
+end
