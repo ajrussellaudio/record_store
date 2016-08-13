af5a38c7ad4d10beda4924e83e0f389f76748c43
Alan Russell <ajrussellaudio@gmail.com>
2016-08-13 20:11:06 +0100

    rename some attrs of stock model

--- models/stock.rb
+++ models/stock.rb
@@ -2,31 +2,35 @@ require_relative "../db/sql_runner"
 
 class Stock
 
+  attr_reader :id, :album_id, :format, :buy_price, :sell_price, :current_stock_level, :reorder_threshold
+
   def initialize( options )
-    @id         = options['id'].to_i
-    @album_id   = options['id'].to_i
-    @format     = options['format']
-    @stock      = options['stock'].to_i
-    @threshold  = options['threshold'].to_i
+    @id       = options['id'].to_i
+    @album_id = options['id'].to_i
+    @format   = options['format']
+
     @buy_price  = options['buy_price'].to_i
     @sell_price = options['sell_price'].to_i
+
+    @current_stock_level = options['current_stock_level'].to_i
+    @reorder_threshold   = options['reorder_threshold'].to_i
   end
 
   def save()
     sql = "INSERT INTO stocks (
       album_id,
       format,
-      stock,
-      threshold,
       buy_price,
-      sell_price
+      sell_price,
+      current_stock_level,
+      reorder_threshold
       ) VALUES (
       #{@album_id},
       '#{@format}',
-      #{@stock},
-      #{@threshold},
       #{@buy_price},
-      #{@sell_price}
+      #{@sell_price},
+      #{@current_stock_level},
+      #{@reorder_threshold}
       ) RETURNING * ;"
     stocks = SqlRunner.run( sql )
     @id = stocks['id'].to_i
