fb924ace38f71e3c90395fa646ec54bd4e38fd00
Alan Russell <ajrussellaudio@gmail.com>
2016-08-15 14:39:36 +0100

    add new artist route

--- controllers/artist.rb
+++ controllers/artist.rb
@@ -3,6 +3,16 @@ get '/artists' do
   erb(:'artists/index')
 end
 
+get '/artists/new' do
+  erb(:'artists/new')
+end
+
+post '/artists/new' do
+  new_artist = Artist.new(params)
+  new_artist.save()
+  redirect to '/artists'
+end
+
 get '/artists/:id' do
   @artist = Artist.find(params['id'])
   erb(:'artists/show')
